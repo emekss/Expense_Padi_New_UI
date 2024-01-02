45904b293c2dcf728cff680c567e4d9f99d5e7ea
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-01-02 23:10:43 +0100

    home screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: CustomPageBackGroundPage(
+        curvedShapeColor: Color.fromRGBO(196, 211, 223, 1),
+        backColor: Color.fromRGBO(50, 90, 250, 1),
+        title: 'EXPENSE PADI',
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
