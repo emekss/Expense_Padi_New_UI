80b246c9ec15036650ec30b2ecc43c80218c47b4
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-01-06 23:54:54 +0100

    link card screen updated

--- lib/screen/link_card_screen.dart
+++ lib/screen/link_card_screen.dart
@@ -6,35 +6,58 @@ class LinkCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+        backgroundColor: Colors.deepPurple,
         body: SafeArea(
-      child: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hello, Emeka!',
-                style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Hello, Emeka!',
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white),
+                  ),
+                  const Text(
+                    'Welcome to ExpensePadi',
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                      child: Container(
+                    height: 149,
+                    width: 312,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          // SizedBox(height: 20),
+                          Icon(
+                            Icons.add,
+                            size: 60,
+                          ),
+
+                          Text(
+                            'Link Card',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )),
+                  const SizedBox(height: 10),
+                ],
               ),
-              Text(
-                'Welcome to ExpensePadi',
-                style: TextStyle(fontSize: 15, color: Colors.white),
-              ),
-              SizedBox(height: 20),
-              Center(
-                  child: BalanceCard(
-                title: 'Total Balance',
-              )),
-              SizedBox(height: 10),
-            ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
