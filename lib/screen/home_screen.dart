b40ab23399c522d9e2607ba2234294a01f759cc5
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-01-06 23:47:57 +0100

    bottom nav bar added

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/balance_card.dart';
 import 'package:flutter_application_1/components/income_card.dart';
@@ -10,101 +11,128 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hello, Emeka!',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    'Welcome to ExpensePadi',
-                    style: TextStyle(fontSize: 15, color: Colors.white),
-                  ),
-                  SizedBox(height: 20),
-                  Center(
-                      child: BalanceCard(
-                    title: 'Total Balance',
-                  )),
-                  SizedBox(height: 10),
-                  Text(
-                    'Overview',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [IncomeCard(), ExpenseCard()],
-                  ),
-                  SizedBox(height: 20),
-                  TransactionColumn(),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Transaction',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, color: Colors.white),
-                      ),
-                      Icon(
-                        Icons.arrow_drop_down_rounded,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                  SingleChildScrollView(
-                    child: SizedBox(
-                      height: 700,
-                      child: Column(
-                        children: [
-                          TransactionCard(
-                            color: Colors.green,
-                            label: 'Supermarket',
-                            price: '1200',
-                          ),
-                          SizedBox(height: 10),
-                          TransactionCard(
-                            color: Colors.red,
-                            label: 'Supermarket',
-                            price: '800',
-                          ),
-                          SizedBox(height: 10),
-                          TransactionCard(
-                            color: Colors.green,
-                            label: 'Supermarket',
-                            price: '700',
-                          ),
-                          SizedBox(height: 10),
-                          TransactionCard(
-                            color: Colors.red,
-                            label: 'Supermarket',
-                            price: '200',
-                          ),
-                          SizedBox(height: 10),
-                          TransactionCard(
-                            color: Colors.green,
-                            label: 'Supermarket',
-                            price: '900',
-                          ),
-                          SizedBox(height: 10),
-                        ],
-                      ),
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello, Emeka!',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
+                Text(
+                  'Welcome to ExpensePadi',
+                  style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
+                SizedBox(height: 20),
+                Center(
+                    child: BalanceCard(
+                  title: 'Total Balance',
+                )),
+                SizedBox(height: 10),
+                Text(
+                  'Overview',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [IncomeCard(), ExpenseCard()],
+                ),
+                SizedBox(height: 20),
+                TransactionColumn(),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Transaction',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, color: Colors.white),
                     ),
-                  )
-                ],
-              ),
+                    Icon(
+                      Icons.arrow_drop_down_rounded,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  child: SizedBox(
+                    height: 750,
+                    child: Column(
+                      children: [
+                        TransactionCard(
+                          color: Colors.green,
+                          label: 'Supermarket',
+                          price: '1200',
+                        ),
+                        SizedBox(height: 10),
+                        TransactionCard(
+                          color: Colors.red,
+                          label: 'Supermarket',
+                          price: '800',
+                        ),
+                        SizedBox(height: 10),
+                        TransactionCard(
+                          color: Colors.green,
+                          label: 'Supermarket',
+                          price: '700',
+                        ),
+                        SizedBox(height: 10),
+                        TransactionCard(
+                          color: Colors.red,
+                          label: 'Supermarket',
+                          price: '200',
+                        ),
+                        SizedBox(height: 10),
+                        TransactionCard(
+                          color: Colors.green,
+                          label: 'Supermarket',
+                          price: '900',
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+          buttonBackgroundColor: Colors.white,
+          color: Colors.white,
+          backgroundColor: Colors.deepPurple,
+          items: const [
+            Icon(
+              Icons.home,
+              color: Colors.deepPurple,
+            ),
+            Icon(
+              Icons.bar_chart_rounded,
+              color: Colors.deepPurple,
+            ),
+            Icon(
+              Icons.add,
+              color: Colors.deepPurple,
+            ),
+            Icon(
+              Icons.wallet,
+              color: Colors.deepPurple,
+            ),
+            Icon(
+              Icons.settings,
+              color: Colors.deepPurple,
+            ),
+          ]),
+    );
   }
 }
